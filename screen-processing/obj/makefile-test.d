920ad5c42a7ba2fb0db9940c37ac6cb6c074e1e4
Ethan Hammond <ethanhammond12099@gmail.com>
2016-11-21 10:20:49 -0500

    Set up dev branch for c++ development with Makefile

---
+++ screen-processing/obj/makefile-test.d
@@ -0,0 +1,2 @@
+obj/makefile-test.o: src/makefile-test.cpp
+src/makefile-test.cpp:
---
+++ screen-processing/src/makefile-test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Makefile test success\n";
+}
